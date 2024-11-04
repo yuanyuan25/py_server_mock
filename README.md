5509f72d4b08ca7afa433b68e51f80f774d32b0e
real <real@mac.local>
2024-11-04 10:06:02 +0800

    * 添加说明信息,修改部署脚本

---
+++ README.md
@@ -0,0 +1,23 @@
+# 使用说明
+
+## 环境
+
+pip install -r requirements.txt
+
+## 部署
+
+./run_mock_server.sh
+> 如果需要后台部署，修改脚本，切换到nohup即可
+
+## 请求链接
+
+依据run_mock_server.sh中的端口确定，默认为18000
+> gunicorn -c gunicorn.py run_mock_server:app -b 0.0.0.0:18000 -t 600000 --timeout 600000 -k gevent  
+
+### 本地地址
+
+http://127.0.0.1:18000/mock  
+
+### 远程地址
+
+http://{部署机器ip}:18000/mock  
--- run_i2i_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo 'Stop services...'
-ps -ux | grep run_i2i_server:app | awk '{print $2}' | xargs kill -9
-echo 'Restart services...'
-#nohup gunicorn -c gunicorn.py --chdir=/home/yuanyuan3/projects/query_emb/server  run_qe_server:app -b 0.0.0.0:18000 -t 600000 --timeout 600000 -k gevent >> ../log/qe_server.log 2>&1 &
-#gunicorn -c gunicorn.py --chdir=/home/yuanyuan3/projects/query_emb/server  run_qe_server:app -b 0.0.0.0:18000 -t 600000 --timeout 600000 -k gevent >> ../log/qe_server.log 2>&1 &
-gunicorn -c gunicorn.py --chdir=/home/yuanyuan3/projects/query_embedding/server  run_i2i_server:app -b 0.0.0.0:19000 -t 600000 --timeout 600000 -k gevent 
-echo 'Done'
